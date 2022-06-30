78dc7f3b6d4136f9989b3999074b37dde73b5a75
Dempsey Tascon <dempseytascon@Dempseys-Mini.broadband>
2022-07-01 00:14:38 +0100

    people tables complete

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -6,7 +6,7 @@ app.use(express.json()); // => req.body
 
 // ROUTES//
 
-//get all values
+//get all people
 
 app.get("/people", async(req, res) => {
     try{
@@ -18,17 +18,27 @@ app.get("/people", async(req, res) => {
     }
 })
 
-//get a value
+//get a person by ID
 
-//create a value
+app.get("/people/:id", async (req,res) => {
+    const {id} = req.params;
+    try {
+        const getPerson = await pool.query("SELECT * FROM people WHERE id = ($1)", [id]);
+        res.json(getPerson.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//add a person
 
 app.post("/people", async(req, res) => {
     try{
         //await
-        const {firstName, secondName, Email } = req.body;
+        const {first_name, secondName, Email } = req.body;
         const newPerson = await pool.query(
             "INSERT INTO people (first_name, second_name, email) VALUES ($1, $2, $3)",
-            [firstName, secondName, Email]
+            [first_name, secondName, Email]
             );
         res.json(newPerson);
     }catch (err){
@@ -36,9 +46,30 @@ app.post("/people", async(req, res) => {
     }
 })
 
-//update a value
+//update a person points
+app.put("/people/:id", async(req, res) => {
+    try{
+        //await
+        const {id} = req.params;
+        const {newPoints} = req.body;
+        const changePerson = await pool.query("UPDATE people SET point = ($1) WHERE id = ($2)", [newPoints, id]);
+        res.json(id + " was updated with new points value "+ newPoints);
+    }catch (err){
+        console.log(err.message);
+    }
+})
 
-//
+// delete a person by ID
+app.put("/people/:id", async(req, res) => {
+    try{
+        //await
+        const {id} = req.params;
+        const deletePerson = await pool.query("DELETE FROM people  WHERE id = ($1)", [id]);
+        res.json(id + " was deleted");
+    }catch (err){
+        console.log(err.message);
+    }
+})
 
 app.listen(3000, () => {
     console.log("server is listening on port 3000");
